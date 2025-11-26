82fd13da0ab8eb725a250adbc82f032a00cd0b48
wingdeans <66850754+wingdeans@users.noreply.github.com>
2025-11-26 10:41:57 -0500

    typst compilation and fragment extraction

--- build.py
+++ build.py
@@ -1,12 +1,30 @@
-import os
 import shutil
+import subprocess
+from glob import glob
+from pathlib import Path
+
+d = Path(__file__).parent.absolute()
 
 def outdated(src, dst):
     try:
-        dtime = os.path.getmtime(dst)
+        dtime = Path(dst).stat().st_mtime
     except OSError:
         return True
-    return dtime < os.path.getmtime(src)
+    return dtime < Path(src).stat().st_mtime
 
-if outdated(src := "src/index.html", dst := "docs/index.html"):
+if outdated(src := d/"src/index.html", dst := d/"docs/index.html"):
     shutil.copyfile(src, dst)
+
+for src in d.glob("src/*.typ"):
+    dst = d / "docs" / Path(src.name).with_suffix(".html")
+    html = subprocess.run(
+        [
+            "typst", "compile",
+            "--features=html", "--format=html",
+            src, "-"
+        ],
+        capture_output=True
+    ).stdout
+
+    with open(dst, "wb") as f:
+        f.write(html[html.index(b"<body>")+6 : html.rindex(b"</body>")])
